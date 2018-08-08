6b7da8b227aa7aeae33ebd6be7e06182e0b4cda7
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-08-08 11:54:57 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],

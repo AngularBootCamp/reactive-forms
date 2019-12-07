a8ff53d66f61355f5cd05b72dd671c268657a235
Zachary Kipping <zachary.kipping@oasisdigital.com>
2019-12-06 21:56:18 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   details: FormGroup;
